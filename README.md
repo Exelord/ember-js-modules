a9b45faaf0fc81fea9732c2cfb4049493601d7c5
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-03-31 17:18:21 +0200

    Upgrade to ember 3.0

--- README.md
+++ README.md
@@ -34,23 +34,29 @@ import { abs } from 'javascript/Math';
 
 ### Installation
 
-* `git clone https://github.com/Exelord/ember-js-modules` this repository
+* `git clone https://github.com/Exelord/ember-js-modules`
 * `cd ember-js-modules`
 * `npm install`
 
-### Running
+### Linting
+
+* `npm run lint:js`
+* `npm run lint:js -- --fix`
+
+### Running tests
+
+* `ember test` – Runs the test suite on the current Ember version
+* `ember test --server` – Runs the test suite in "watch mode"
+* `npm test` – Runs `ember try:each` to test your addon against multiple Ember versions
+
+### Running the dummy application
 
 * `ember serve`
-* Visit your app at [http://localhost:4200](http://localhost:4200).
-
-### Running Tests
-
-* `npm test` (Runs `ember try:each` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
-
-### Building
-
-* `ember build`
+* Visit the dummy application at [http://localhost:4200](http://localhost:4200).
 
 For more information on using ember-cli, visit [https://ember-cli.com/](https://ember-cli.com/).
+
+License
+------------------------------------------------------------------------------
+
+This project is licensed under the [MIT License](LICENSE.md).
--- tests/unit/javascript_test.js
+++ tests/unit/javascript_test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'ember-qunit';
+import { module, test } from 'qunit';
 
 import JavaScript from 'javascript';
 import { Object as JSObject } from 'javascript';
@@ -6,20 +6,20 @@ import { Object as JSObject } from 'javascript';
 import JSMath from 'javascript/Math';
 import { isArray } from 'javascript/Array';
 
-module('javascript', 'Unit | Shim | javascript');
+module('Unit | Shim | javascript', function() {
+  test('it imports self object', function(assert) {
+    assert.equal(JavaScript, self);
+  });
 
-test('it imports self object', function(assert) {
-  assert.equal(JavaScript, self);
-});
+  test('it can imports self modules', function(assert) {
+    assert.equal(JSMath, self.Math);
+  });
 
-test('it can imports self modules', function(assert) {
-  assert.equal(JSMath, self.Math);
-});
+  test('it can destructure modules', function(assert) {
+    assert.equal(isArray, self.Array.isArray);
+  });
 
-test('it can destructure modules', function(assert) {
-  assert.equal(isArray, self.Array.isArray);
-});
-
-test('it can destructure self object', function(assert) {
-  assert.equal(JSObject, self.Object);
+  test('it can destructure self object', function(assert) {
+    assert.equal(JSObject, self.Object);
+  });
 });
