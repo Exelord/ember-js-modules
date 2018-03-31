059c6a901dfaf17ca3ae2e53eb5466a9f85e3c5a
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-03-31 17:38:30 +0200

    Fix options

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 module.exports = {
   name: 'ember-js-modules',
 
-  options: {
-    babel: {
-      plugins: ['javascript']
-    }
+  included(app, parentAddon) {
+    let target = (parentAddon || app);
+
+    target.options = target.options || {};
+    target.options.babel = target.options.babel || {};
+    target.options.babel.plugins = target.options.babel.plugins || [];
+    target.options.babel.plugins.push('javascript');
+
+    return this._super.included.apply(this, arguments);
   }
 };
