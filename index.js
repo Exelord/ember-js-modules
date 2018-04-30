82643ae72ef369b3ed88308e2f743f3c764b8315
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-04-30 16:17:20 +0300

    Include plugin in babel options

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
 module.exports = {
   name: 'ember-js-modules',
 
-  included(parent) {
-    parent.options = parent.options || {};
-    parent.options.babel = parent.options.babel || {};
-    parent.options.babel.plugins = parent.options.babel.plugins || [];
-    parent.options.babel.plugins.push('javascript');
-
-    return this._super.included.apply(this, arguments);
+  options: {
+    babel: {
+      plugins: ['javascript']
+    }
   }
 };
